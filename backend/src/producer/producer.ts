0f9a9c39cfe9f775cddb7c23bc7c2aefad43ca4e
radush-radency <vladymyr.r@radency.com>
2022-03-12 21:57:31 +0200

    rabbitMQ connection completed

--- backend/src/producer/producer.ts
+++ backend/src/producer/producer.ts
@@ -3,48 +3,21 @@ import amqp from 'amqplib';
 
 const producer = express();
 
-const rabbitMqSettings = {
-    protocol: 'amqp',
-    hostname: 'localhost',
-    port: 5672,
-    username: process.env.RABBITMQ_DEFAULT_USER,
-    password: process.env.RABBITMQ_DEFAULT_PASSWORD,
-    vhost: '/',
-    authMechanism:['PLAIN','AMQPLAIN','EXTERNAL']
-}
+const login = process.env.USER;
+const password = process.env.PASSWORD;
+const port = process.env.CLIENT_PORT
+
+const link = `amqp://${password}:${login}@rabbitmq:${port}`
 
 const sendRabbitMq = async () => {
     try{
-        const conn = await amqp.connect(rabbitMqSettings);
+        const conn = await amqp.connect(link);
         console.log("Done");
-        
     }
     catch(err){
         console.log(err);
     }
 }
-
-// const sendRabbitMq = () => {
-//     amqp.connect('amqp://localhost', function (error0, connection) {
-//         if (error0) {
-//             throw error0;
-//         }
-//         connection.createChannel(function (error1, channel) {
-//             if (error1) {
-//                 throw error1;
-//             }
-//             var queue = 'hello';
-//             var msg = 'Hello world';
-
-//             channel.assertQueue(queue, {
-//                 durable: false
-//             });
-
-//             channel.sendToQueue(queue, Buffer.from(msg));
-//             console.log(" [x] Sent %s", msg);
-//         });
-//     });
-// }
 
 producer.post('/send', (_req, res) => {
     sendRabbitMq();
